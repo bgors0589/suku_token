3983095712128eee7ffd747b91b270ba7d2a9f7e
bgors0589 <62038882+bgors0589@users.noreply.github.com>
2021-12-03 17:22:36 -0500

    Update WhiteListable.sol

--- contracts/WhiteListable.sol
+++ contracts/WhiteListable.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.0;
+pragma solidity ^0.5.0;
 
 import "https://github.com/bgors0589/openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/bgors0589/openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
