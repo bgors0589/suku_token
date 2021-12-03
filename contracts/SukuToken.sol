45ae197eed8f513bdb4fa1b367c75b6257fb2399
bgors0589 <62038882+bgors0589@users.noreply.github.com>
2021-12-03 12:43:44 -0500

    Update SukuToken.sol

--- contracts/SukuToken.sol
+++ contracts/SukuToken.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.5.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/bgors0589/openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/bgors0589/openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "./Whitelistable.sol";
 import "./Restrictable.sol";
 import "./ERC1404.sol";
